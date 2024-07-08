385e321ab18be6106aa8d8fffbb400c0f745cc34
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-07-08 15:05:34 +0300

    update cors

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8787;
+const cors = require("cors")
 
 const { messageText, whatsapp } = require("./whatsapp");
 const { errorHandler } = require('./middleware/error-handler.mw');
 
 app.use(express.json())
+const allowedOrigins = [corsOrigin, corsDev];
+app.use(cors<Request>({
+    origin: 
+    function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    //process.env.NODE_ENV === 'production' ? corsOrigin : corsDev,
+    methods: ["POST", "GET", "DELETE", "PATCH"],
+}));
 
 app.post("/send-message", async (req, res, next) => {
     try {
